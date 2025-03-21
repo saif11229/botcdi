34838613c4f35a8b1854b0157336a583546aeb3c
ewrsaif571 <saifewr6@gmail.com>
2025-03-21 16:20:23 +0600

    Update balance.js

--- scripts/cmds/balance.js
+++ scripts/cmds/balance.js
@@ -2,7 +2,7 @@ module.exports = {
 	config: {
 		name: "balance",
 		aliases: ["bal"],
-		version: "1.2",
+		version: "1.3",
 		author: "NTKhang",
 		countDown: 5,
 		role: 0,
@@ -30,17 +30,31 @@ module.exports = {
 		}
 	},
 
+	// Function to format numbers into K, M, B, T, etc.
+	formatMoney: function (num) {
+		const units = ["", "K", "M", "B", "T", "Q", "Qt", "Sx", "Sp", "O", "N"];
+		let unitIndex = 0;
+		while (num >= 1000 && unitIndex < units.length - 1) {
+			num /= 1000;
+			unitIndex++;
+		}
+		return `${num.toFixed(2)}${units[unitIndex]}`;
+	},
+
 	onStart: async function ({ message, usersData, event, getLang }) {
 		if (Object.keys(event.mentions).length > 0) {
 			const uids = Object.keys(event.mentions);
 			let msg = "";
 			for (const uid of uids) {
 				const userMoney = await usersData.get(uid, "money");
-				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), userMoney) + '\n';
+				const formattedMoney = this.formatMoney(userMoney);
+				msg += getLang("moneyOf", event.mentions[uid].replace("@", ""), formattedMoney) + '\n';
 			}
 			return message.reply(msg);
 		}
+
 		const userData = await usersData.get(event.senderID);
-		message.reply(getLang("money", userData.money));
+		const formattedMoney = this.formatMoney(userData.money);
+		message.reply(getLang("money", formattedMoney));
 	}
 };
